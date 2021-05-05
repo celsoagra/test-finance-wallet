6ddcce4a7c06ea37fd3619b42e0a8bc0221b5c59
Celso Agra <celso.agra@compasso.com.br>
2021-05-05 01:04:13 -0300

    add more cases

--- src/main/java/io/celsoagra/finance/start/Initializer.java
+++ src/main/java/io/celsoagra/finance/start/Initializer.java
@@ -31,30 +31,42 @@ public class Initializer {
         
         Wallet walletA = new Wallet();
         Wallet walletB = new Wallet();
+        Wallet walletC = new Wallet();
         
         log.info("coinbase ID: {}", coinbase);
         log.info("walletA ID: {}", walletA.getPublicKeyAsString());
         log.info("walletB ID: {}", walletB.getPublicKeyAsString());
+        log.info("walletC ID: {}", walletC.getPublicKeyAsString());
         
         log.info("coinbase balance: {}", client.balance(CoinbaseDTO.newInstance(coinbase)).getBody() );
         log.info("walletA balance: {}", client.balance(CoinbaseDTO.newInstance(walletA.getPublicKeyAsString())).getBody() );
         log.info("walletB balance: {}", client.balance(CoinbaseDTO.newInstance(walletB.getPublicKeyAsString())).getBody() );
+        log.info("walletC balance: {}", client.balance(CoinbaseDTO.newInstance(walletC.getPublicKeyAsString())).getBody() );
         
         log.info("walletA faucet: {}", client.faucet(CoinbaseDTO.newInstance(walletA.getPublicKeyAsString())).getBody() );
         log.info("walletB faucet: {}", client.faucet(CoinbaseDTO.newInstance(walletB.getPublicKeyAsString())).getBody() );
+        log.info("walletC faucet: {}", client.faucet(CoinbaseDTO.newInstance(walletC.getPublicKeyAsString())).getBody() );
+        log.info("walletC faucet: {}", client.faucet(CoinbaseDTO.newInstance(walletC.getPublicKeyAsString())).getBody() );
         
         log.info("coinbase balance: {}", client.balance(CoinbaseDTO.newInstance(coinbase)).getBody() );
         log.info("walletA balance: {}", client.balance(CoinbaseDTO.newInstance(walletA.getPublicKeyAsString())).getBody() );
         log.info("walletB balance: {}", client.balance(CoinbaseDTO.newInstance(walletB.getPublicKeyAsString())).getBody() );
+        log.info("walletC balance: {}", client.balance(CoinbaseDTO.newInstance(walletC.getPublicKeyAsString())).getBody() );
         
         double valueToSend = 3; 
         TransactionDTO sendAToB = TransactionDTO.builder().sender(walletA.getPublicKeyAsString()).receiver(walletB.getPublicKeyAsString()).value(valueToSend).build();
         sendAToB.setSignature( walletA.generateSignature(walletB.getPublicKeyAsString(), valueToSend) );
-        log.info("send 3 buck from walletA to walletB: {}", client.send(sendAToB).getBody() );
+        log.info("send 3 bucks from walletA to walletB: {}", client.send(sendAToB).getBody() );
+        
+        double valueToSendFromC = 7.00005d; 
+        TransactionDTO sendCToB = TransactionDTO.builder().sender(walletC.getPublicKeyAsString()).receiver(walletB.getPublicKeyAsString()).value(valueToSendFromC).build();
+        sendCToB.setSignature( walletC.generateSignature(walletB.getPublicKeyAsString(), valueToSendFromC) );
+        log.info("send bucks from walletC to walletB: {}", client.send(sendCToB).getBody() );
         
         log.info("coinbase balance: {}", client.balance(CoinbaseDTO.newInstance(coinbase)).getBody() );
         log.info("walletA balance: {}", client.balance(CoinbaseDTO.newInstance(walletA.getPublicKeyAsString())).getBody() );
         log.info("walletB balance: {}", client.balance(CoinbaseDTO.newInstance(walletB.getPublicKeyAsString())).getBody() );
+        log.info("walletC balance: {}", client.balance(CoinbaseDTO.newInstance(walletC.getPublicKeyAsString())).getBody() );
         
     }
 
